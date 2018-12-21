4fb19b448b74f6c459505eaeb941922616e57f6b
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-12-21 15:43:00 +0300

    rework screenshots carousel: btns aare positioned outside, the animation wave comes after page loading

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -2,6 +2,10 @@ import 'bootstrap';
 
 import Swiper from 'swiper';
 
+const Classes = {
+  WINDOW_HOVER: 'jumbotron__window-link--hover'
+}
+
 $( document ).ready(() => {
   Array.from(document.querySelectorAll('#partners-carousel')).forEach(n => new Swiper(n, {
     loop: true,
@@ -10,8 +14,8 @@ $( document ).ready(() => {
     initialSlide: 1,
     // autoplay: true,
     navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next',
+      prevEl: '.partners-button-prev',
+      nextEl: '.partners-button-next',
     },
   }));
 
@@ -21,8 +25,8 @@ $( document ).ready(() => {
     initialSlide: 1,
     // autoplay: true,
     navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next',
+      prevEl: '.screenshots-button-prev',
+      nextEl: '.screenshots-button-next',
     },
   });
 
@@ -35,6 +39,40 @@ $( document ).ready(() => {
 
     return;
   });
+
+  const carouselItems = $('data-carousel-item');
+
+  setTimeout(() => {
+    const toggleHoverClass = (boolean) => {
+      return function() {
+        boolean ? $( this ).addClass(Classes.WINDOW_HOVER) : $( this ).removeClass(Classes.WINDOW_HOVER);
+      };
+    }
+
+    const $items = $('[data-carousel-item]');
+
+    if(!$items.length) return;
+
+    $items.one('mouseenter', toggleHoverClass(true));
+
+    $items.one('mouseleave', function() {
+      setTimeout(toggleHoverClass(false).bind(this), 150);
+    });
+
+    const [item, ...items] = Array.from($items);
+
+    const rec = (cur, arr) => {
+      $( cur ).trigger('mouseenter').trigger('mouseleave');
+
+      if(!arr.length) return;
+
+      const [first, ...rest] = arr;
+
+      return setTimeout(() => rec(first, rest), 75);
+    }
+
+    rec(item, items);
+  }, 1500);
 
   ;(function (window, document, undefined) {
       "use strict";
